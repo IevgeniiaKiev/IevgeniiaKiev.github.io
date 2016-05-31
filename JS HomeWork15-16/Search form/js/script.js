b8dc4281033edb33f00f7d84199dd337d6b860f0
Ievgeniia <ievgeniia.semenova@gmail.com>
2016-05-31 17:59:34 +0300

    changes JS Home Work 15-16

--- JS HomeWork15-16/Search form/js/script.js
+++ JS HomeWork15-16/Search form/js/script.js
@@ -1,76 +1,35 @@
 $(function() {
 
-var API_KEY = '2625359-f397bd2a457159fc8090d8204';
+$('#searchButton').on('click', runSearch);
 
-var limit = 12;
-
-var minHeight = 250;
-var minWidth = 250;
-
-var currentIndex = 0;
-var currentTerm = "";
-
-var searchButton = $('#searchButton');
-var searchInput = $('#searchInput');
-var searcResult = $('body').append('<div class="searchResult">');
-
-searchButton.click(function() {
-      runSearch(searchInput.val(), 0);
-});
-
-searchInput.keydown(function(event) {
-        if(event.keyCode == 13) {
-            runSearch(searchInput.val(), 0);
-        }
-});
-
-function runSearch(term, index) {
-    currentTerm = term;
-    currentIndex = index;
-    var page = index+1;
-    var url = "https://pixabay.com/api/?key="+API_KEY
-                +"&q="+encodeURIComponent(term)
-                +"&per_page="+limit
-                +"&page="+page
-                +"&min_width="+minWidth
-                +"&minHeight="+minHeight;
-
-    $.ajax({
-        url: url,
-        success: searchComplete,
-        error: searchFail,
-        dataType: "json"
+$('#searchInput').keydown(function(event) {
+        if (event.keyCode === 13) {
+            runSearch;
+        };
     });
-}
 
-function searchComplete(json) {
+    function runSearch(e) {
+        e.preventDefault();
+        $('img').remove();
+        var searchKey = $('#searchInput').val();
 
-    var totalResults = json.totalHits;
-    var contentDiv = $('#searchResult').empty();
+        $.ajax({
+            url: 'https://pixabay.com/api/?key=2625359-f397bd2a457159fc8090d8204&q=' + searchKey + '&image_type=photo',
+            dataType: 'jsonp',
+            success: function (data) {
+                var result = data.hits;
 
-    var results = json.hits;
+               if (data.hits == 0) {
+                   $('.searchResult').append('<p>Nothing was found!</p>');
+                 }
 
-    if (results.length == 0)
-        $("<p>Nothing found</p>").appendTo(contentDiv);
-
-    for (var i = 0; i < results.length; i++) {
-        var result = results[i];
-
-        var imgContainer = $('<div class="imgContainer">');
-        var newImg = document.createElement('img');
-
-        newImg.src = result.previewURL;
-
-        var imgWidth = (result.previewWidth > minWidth)?result.previewWidth:minWidth;
-        var imgHeight = (result.previewHeight > minHeight)?result.previewHeight:minHeight;
-        imgContainer.width(imgWidth).height(imgHeight);
-        imgContainer.append($(newImg));
-        imgContainer.appendTo(contentDiv);
-}
-};
-
-function searchFail(jqXHR, textStatus, errorThrown) {
-    alert('Couldn\'t connect to Pixabay: ' + textStatus + ' ' + errorThrown);
-}
-
+                for (var i = 0; i < data.hits.length; i++) {
+                    $('.searchResult').append('<img class="img_result" src="' + result[i].webformatURL + '">');
+                }
+            },
+            error: function () {
+                alert('Error!');
+            }
+        });
+    }
 });

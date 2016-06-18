997b4c5fe78926155c9e26c37f7ae93cff90a696
Ievgeniia <ievgeniia.semenova@gmail.com>
2016-06-18 17:07:58 +0300

    JS HomeWork 13-14

--- JS HomeWork13-14/js/script.test.js
+++ JS HomeWork13-14/js/script.test.js
@@ -14,7 +14,8 @@ for (var k = 0; k < newTest.testQuestions.length; k++) {
         rightAnswer[k] = newTest.testQuestions[k].correctAnswer;
     }
 
-$(':submit').click(function() {
+$(':submit').click(function(event) {
+  event.preventDefault();
   var checkedAnswer = $('input:checked');
   var compare = [];
   for (var j = 0; j < checkedAnswer.length; j++) {
@@ -29,12 +30,12 @@ var result;
   }
 
 $('body').append('<div class="modal fade" id="basicModal" tabindex="-1" role="dialog"><div class="modal-dialog"><div class="modal-content"><div class="modal-header"><button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button><h4 class="modal-title">Результат прохождения теста</h4></div><div class="modal-body"><p>'+ result +'</p></div><div class="modal-footer"><button type="button" class="btn btn-default" data-dismiss="modal" id="closeButton">Закрыть</button></div></div></div></div>');
-$(':submit').click(function() {
+$(':submit').click(function(event) {
+  event.preventDefault();
   $('#basicModal').modal();
 });
 $('#closeButton').click(function() {
   $(checkedAnswer).prop('checked', false);
-                $(".modal").filter('fade').remove();
 });
 });
 });
